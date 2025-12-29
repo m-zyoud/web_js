ff89c02b3be99abd7288af0d44bbce3a5e97c037
m-zyoud <mohammadzyoud720@gmail.com>
2025-12-29 21:34:28 +0200

    todo-app-config

---
+++ main.js
@@ -0,0 +1,9 @@
+const STORAGE_KEYS = {
+    TASKS: 'todoApp_tasks',
+    LANGUAGE: 'todoApp_language',
+    THEME: 'todoApp_theme',
+    FILTER: 'todoApp_filter'
+};
+
+let currentLanguage = 'en';
+let currentTheme = 'light';

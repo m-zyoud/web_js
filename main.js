005abf717e539365d8e4a421dde47f460d0ac2cb
Ras7id6 <147885117+Ras7id6@users.noreply.github.com>
2025-12-31 23:30:51 +0200

    commit32

--- main.js
+++ main.js
@@ -297,3 +297,15 @@ filteredTasks.forEach((task, index) => {
         renameIndex = null;
         document.getElementById('renamePopup').style.display = 'none';
       }
+      function deleteDoneTasks() {
+        const doneCount = tasks.filter(task => task.done).length;
+        if (doneCount === 0) {
+          alert(getText('noDoneTasks'));
+          return;
+        }
+        confirmPopup(() => {
+          tasks = tasks.filter(task => !task.done);
+          saveToStorage();
+          renderTasks();
+        }, getText('deleteDoneConfirm', { count: doneCount }));
+      }
